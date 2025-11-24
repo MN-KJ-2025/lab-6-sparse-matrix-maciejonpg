35daddb40ef7af4b7e5e4913a4a19c5cc6c28b41
maciejonpg <npgmaciejo@gmail.com>
2025-11-24 10:54:58 +0100

    sprawozdanie lab6

--- main.py
+++ main.py
@@ -9,35 +9,51 @@
 # =============================================================================
 import numpy as np
 import scipy as sp
+from scipy.sparse import issparse
+
 
 
 def is_diagonally_dominant(A: np.ndarray | sp.sparse.csc_array) -> bool | None:
-    """Funkcja sprawdzająca czy podana macierz jest diagonalnie zdominowana.
-
-    Args:
-        A (np.ndarray | sp.sparse.csc_array): Macierz A (m,m) podlegająca 
-            weryfikacji.
     
-    Returns:
-        (bool): `True`, jeśli macierz jest diagonalnie zdominowana, 
-            w przeciwnym wypadku `False`.
-        Jeżeli dane wejściowe są niepoprawne funkcja zwraca `None`.
-    """
-    pass
+    if not isinstance(A, (np.ndarray, sp.sparse.csc_array)):
+        return None
+
+    if A.ndim != 2:
+        return None
+
+    rows, cols = A.shape
+    if rows != cols:
+        return None
+    
+    if not np.issubdtype(A.dtype, np.number):
+        return None
+
+    if issparse(A):
+        A = A.toarray()
+
+    
+    diag_abs = np.abs(np.diag(A))
+
+
+    row_abs_sums = np.sum(np.abs(A), axis=1) - diag_abs
+
+    return bool(np.all(diag_abs > row_abs_sums))
+
 
 
 def residual_norm(A: np.ndarray, x: np.ndarray, b: np.ndarray) -> float | None:
-    """Funkcja obliczająca normę residuum dla równania postaci: 
-    Ax = b.
 
-    Args:
-        A (np.ndarray): Macierz A (m,n) zawierająca współczynniki równania.
-        x (np.ndarray): Wektor x (n,) zawierający rozwiązania równania.
-        b (np.ndarray): Wektor b (m,) zawierający współczynniki po prawej 
-            stronie równania.
+    if not isinstance(A, np.ndarray) or not isinstance(x, np.ndarray) or not isinstance(b, np.ndarray):
+        return None
     
-    Returns:
-        (float): Wartość normy residuum dla podanych parametrów.
-        Jeżeli dane wejściowe są niepoprawne funkcja zwraca `None`.
-    """
-    pass
+    if x.ndim != 1 or b.ndim != 1:
+        return None
+
+    m, n = A.shape
+    if n != x.shape[0] or m != b.shape[0]:
+        return None
+
+    residual = b - A @ x
+    res_norm = np.linalg.norm(residual)
+
+    return float(res_norm)
